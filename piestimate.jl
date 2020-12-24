f8590b4f334ccc8576965c386a4f351db69d15c7
Ben Jiron <66054297+benharmonics@users.noreply.github.com>
2020-12-23 21:20:55 -0700

    Add Julia file

---
+++ piestimate.jl
@@ -0,0 +1,23 @@
+let
+MAX_ITER = 1_500_000_000 
+hit_count = 0
+iter_count = 0
+
+for i ∈ 1:MAX_ITER
+    x = rand()
+    y = rand()
+    iter_count += 1
+    if x^2 + y^2 < 1
+        hit_count += 1
+    end
+    if iter_count % 20_000_000 == 0
+        global estimate = 4*hit_count/iter_count
+        if iter_count < 1_000_000_000
+            println("Current estimate: $(round(estimate, digits=8)) after $(iter_count/1_000_000) million iterations.")
+        elseif iter_count < MAX_ITER
+            println("Current estimate: $(round(estimate, digits=8)) after $(iter_count/1_000_000_000) billion iterations.")
+        end
+    end
+end
+println("\nFinal estimate: $estimate after $(iter_count/1_000_000_000) billion iterations.")
+end
